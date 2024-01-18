8c1a202eed04f004b6ad45ac3a70ddad0582135a
Jordan Reed <jordan.reed.aws@gmail.com>
2024-01-18 12:46:17 -0500

    udpate branch mapping to print workspace name

--- .github/workflows/utils/branch_mapping.py
+++ .github/workflows/utils/branch_mapping.py
@@ -9,10 +9,16 @@ def map_branch_name(branch_name):
         return "test"
     
     elif re.match(r'(feature|bugfix|task|hotfix)/\w+', branch_name):
-        return f"{branch_name[0]}-{branch_name.split('/')[1]}"
+        """
+        takes first letter of branch and concats it using hyphen with the branch description
+        i.e. feature/test (branch name) -> f-test (workspace name)
+        """
+        return f"{branch_name[0]}-{branch_name.split('/')[1]}" 
+        
     
     else:
         print("There is no mapping for this branch name format, please try again")
+        exit(1)
         
 workspace_name = map_branch_name(sys.argv[1])
-# print(workspace_name)
+print(f"Workspace nameworkspace_name: {workspace_name}")
