b7f417ddfa6fd0e18d485c91d0bc33af3b415249
Jordan Reed <jordan.reed.aws@gmail.com>
2024-01-18 12:41:01 -0500

    udpate branch mapping to use parameter

--- .github/workflows/utils/branch_mapping.py
+++ .github/workflows/utils/branch_mapping.py
@@ -1,4 +1,5 @@
 import re
+import sys
 
 def map_branch_name(branch_name):
     if branch_name == "main":
@@ -8,10 +9,10 @@ def map_branch_name(branch_name):
         return "test"
     
     elif re.match(r'(feature|bugfix|task|hotfix)/\w+', branch_name):
-        return f"{branch_name[0]}-{branch_name.split('/')[1][0:4]}"
+        return f"{branch_name[0]}-{branch_name.split('/')[1]}"
     
     else:
         print("There is no mapping for this branch name format, please try again")
         
-workspace_name = map_branch_name("fds/test")
-print(workspace_name)
+workspace_name = map_branch_name(sys.argv[1])
+# print(workspace_name)
