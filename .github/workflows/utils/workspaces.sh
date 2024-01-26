7aa9f1cdcffb80b03635f6c8b30fd5a7ae7396dd
Jordan Reed <jordan.reed.aws@gmail.com>
2024-01-26 11:42:04 -0500

    clean up workflows and utils

--- .github/workflows/utils/workspaces.sh
+++ .github/workflows/utils/workspaces.sh
@@ -13,6 +13,7 @@ found=false
 echo "Branch name: $BRANCH_NAME"
 export WORKSPACE_NAME=$(python ../../.github/workflows/utils/branch_mapping.py "$BRANCH_NAME")
 
+# find if workspace exists already
 for item in "${workspace_list[@]}"; do
     if [[ "$item" == $WORKSPACE_NAME ]]; then
         found=true
@@ -20,6 +21,7 @@ for item in "${workspace_list[@]}"; do
     fi
 done
 
+# switch to or create workspace
 if [ "$found" = true ]; then
     echo "Switching to workspace '$WORKSPACE_NAME'."
     terraform workspace select $WORKSPACE_NAME
